6962de89dc73671ffaf616f992fd0318abacf2d6
matthiku <matthiku@yahoo.com>
2018-03-06 19:14:02 +0000

    working on item 1 of issue #5

--- resources/assets/js/components/Plan/View/Presentation.vue
+++ resources/assets/js/components/Plan/View/Presentation.vue
@@ -1,20 +1,36 @@
 <template>
   <v-container fluid>
-    <div v-if="plan" v-for="item in plan.actionList" :key="item.seqNo">
-      <div v-if="item.seqNo===stage"
-          class="fullwindow"
-          @click="stage++">
-        {{ item.title }}
+
+    <div v-if="plan" v-for="item in plan.actionList"
+        :key="item.seqNo"
+        class="fullwindow"
+        @click.left="showNext()"
+        @click.right="showNext(-1)"
+      >
+
+      <!-- show only current item -->
+      <div v-if="item.seqNo===showSeqNo"
+        >
+
+        <!-- SONG -->
+        <span v-if="item.type==='song'">
+          <h3>{{ item.title }}</h3>
+          <pre>{{ item.lyrics }}</pre>
+        </span>
+
+        <!-- READING -->
+        <span v-if="item.type==='read'">
+          <pre>{{ scriptureRefs[item.title] }}</pre>
+        </span>
+
       </div>
     </div>
+
   </v-container>
 </template>
 
 
-<style scoped>
-html {
-  overflow-y: hidden;
-}
+<style>
 .fullwindow {
   width: 100%;
   height: 100%;
@@ -33,7 +49,7 @@ export default {
 
   data () {
     return {
-      stage: 1
+      showSeqNo: 1
     }
   },
 
@@ -42,9 +58,42 @@ export default {
       console.log('no plan found!', this.plan)
       this.$router.push('/plans')
     }
+    if (!this.plan.actionList.length) {
+      this.$store.commit('setError', 'Cannot present, this plan has no actions!')
+      this.$router.go(-1) // go back to previous page
+    }
+    // make sure we show the first non-hidden slide
+    this.showNext()
 
+    // remove the scrollbar on the right side
     let page = document.getElementsByTagName('html')[0]
     page.style.overflowY = 'hidden'
+  },
+
+  methods: {
+    showNext(dir) {
+      // default direction is forward, -1 for backwards
+      if (dir === undefined) dir = 1
+      // look for the next non-hidden and non-text activity
+      while (
+        this.plan.actionList[this.showSeqNo].forLeadersEyesOnly || 
+        this.plan.actionList[this.showSeqNo].type === 'text'
+      ) {
+        this.showSeqNo = this.showSeqNo + dir
+        // go no further than the last or the first showable activity
+        if (
+          this.showSeqNo > this.plan.actionList.length ||
+          this.showSeqNo < 1
+        ) break
+      }
+    }
+  },
+
+  destroyed () {
+    // enable the scrollbar again
+    let page = document.getElementsByTagName('html')[0]
+    page.removeAttribute('style')
   }
+
 }
 </script>

6501ddc2ba432694b1539cfa5e1bc999cd164add
Matthias Kuhs <matthiku@yahoo.com>
2018-03-13 16:35:36 +0000

    working on item 3 of issue #5

--- resources/assets/js/components/Plan/Present/PresentLyrics.vue
+++ resources/assets/js/components/Plan/Present/PresentLyrics.vue
@@ -11,7 +11,15 @@
           class="lyrics-slide hidden"
           :class="slideClass"
         >
-        <pre>{{ part }}</pre>
+        <div v-for="(line, index) in part"
+            class="lyrics-line"
+            :key="index"
+          >
+          <span v-if="hasInstructions(line)">
+            <span class="red--text">{{ hasInstructions(line)[0] }}</span>
+            {{ hasInstructions(line)[1] }}
+          </span>
+          <span v-else>{{ line }}</span></div>
       </div>
 
   </span>  
@@ -20,6 +28,9 @@
 <style>
 .lyrics-slide {
   height: 100%;
+}
+.lyrics-line {
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
 }
 </style>
 
@@ -37,7 +48,61 @@ export default {
 
   data () {
     return {
-      verses: []
+      verses: [],
+      isRegion2: false
+    }
+  },
+
+  methods: {
+    isRegionTwo (line) {
+      // search for "REGION 2" in the text line
+      var patt = /\[region\s*2\]/i
+      if (patt.test(line)) {
+        this.isRegion2 = true
+      } else {
+        this.isRegion2 = false
+      }
+      return this.isRegion2
+    },
+    /**
+     * Looks for singing instructions at the beginning of the line indicated by simple brackets (xyz) 
+     * 
+     * returns false if none found or an array with the actual instructions and the rest of the line
+     */
+    hasInstructions (text) {
+      var patt = /^\(.+\)/
+      if (!patt.test(text)) return false
+      // if matching text is found, split the line at the closing bracket
+      let strings = text.split(')')
+      if (!strings.length === 2) return false // not catering for more than 2 instruction blocks...
+      strings[0] = strings[0] + ')'
+      return strings
+    },
+    /**
+     * @description: Create single slides from a block of text (multiple lines) 
+     *               with emptly lines as separators
+     *
+     * @argument block (string) with possibly mulitple lines of text, 
+     *                          possibly containing one or more empty lines
+     *
+     * @returns: (array) slides, each containing an array of text lines
+     */
+    splitByEmptyLine (block) {
+      let output = []
+      let slide = []
+      let lines = block.split('\n')
+      lines.forEach(line => {
+        if (line.trim() !== '') {
+          slide.push(line.trim())
+        } else if (slide.length) {
+          output.push(slide)
+          slide = []
+        } else {
+          slide = []
+        }
+      })
+      if (slide.length) output.push(slide)
+      return output
     }
   },
 
@@ -66,16 +131,25 @@ export default {
       sequenceArr.forEach(seq => {
         // intro, notes or meta parts must be ignored
         if ('smi'.indexOf(seq) >= 0) return
-        if (!this.songParts.hasOwnProperty(seq)) return
+        if (!this.songParts.hasOwnProperty(seq)) return // song part code could not be found!
+
         let partId = this.songParts[seq].id
         if (!this.item.onsongs.hasOwnProperty(partId)) return
+
         // extract lyrics only from the OnSong data
         let lyrics = this.getLyricsFromOnsong(this.item.onsongs[partId].text)
-        //TODO:
-        // 1. How to handle singing insctructions, e.g. 'men' / 'women'
-        // 2. blank lines must be treated as to start a new slide
-        // 3. TWO REGIONS view
-        this.verses.push(lyrics)
+
+        // check if lyrics contain a blank line, indicating a new slide
+        let slides = this.splitByEmptyLine(lyrics)
+        slides.forEach(slide => this.verses.push(slide))
+
+        /** TODO:
+         *
+         * 1. How to handle singing instructions, e.g. '(men)' / '(women)'
+         * 2. TWO REGIONS view, indicated by a line like "[region 2]"
+         * 
+         * => must all be handled in the view (template)
+         */
       })
     }
 
@@ -85,20 +159,22 @@ export default {
       // content which indicates a new slide (e.g. verses numbers or blank lines)
       let song = this.item.lyrics
       // check which character(s) is/are used as line seperator
-      parts = song.split('\n')
-      let slide = ''
-      parts.forEach(line => {
+      let lines = song.split('\n')
+      let slide = []
+      lines.forEach(line => {
         if (line.trim() !== '' && line.indexOf('[') < 0) {
-          slide += line + '\r\n'
-        } else if (slide !== '') {
+          slide.push(line)
+        } else if (slide.length) {
           this.verses.push(slide)
-          slide = ''
+          slide = []
+        } else {
+          slide = []
         }
       })
       // needed if song contains only one line!
-      if (slide) this.verses.push(slide)
+      if (slide.length) this.verses.push(slide)
     }
-    else {
+    if (!this.verses.length) {
       this.verses.push('(no lyrics found for this song!)')
     }
   }
